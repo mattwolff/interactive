fd5907f697b1f543c9dc76f275aa4dd2b7cacd64
Matt Wolff <matt.t.wolff@gmail.com>
2018-02-11 20:06:31 -0500

    added secure http

--- assets/js/boxify.js
+++ assets/js/boxify.js
@@ -5,7 +5,7 @@
 	if (window.jQuery === undefined || window.jQuery.fn.jquery < version) {
 		var done = false;
 		var script = document.createElement("script");
-		script.src = "http://ajax.googleapis.com/ajax/libs/jquery/" + version + "/jquery.min.js";
+		script.src = "https://ajax.googleapis.com/ajax/libs/jquery/" + version + "/jquery.min.js";
 		script.onload = script.onreadystatechange = function(){
 			if (!done && (!this.readyState || this.readyState == "loaded" || this.readyState == "complete")) {
 				done = true;
