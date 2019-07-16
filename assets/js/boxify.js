99189f9bad2765c8826c667125988229eeae1383
Matt <matthew.wolff@yale.edu>
2019-07-16 18:28:44 -0400

    remove jQuery, JS Boxify in Vanilla Js

--- assets/js/boxify.js
+++ assets/js/boxify.js
@@ -1,37 +1,10 @@
 (function(){
-	// Needs jQuery
-	var version = "1.10.2";
-	// Check prior inclusion and version
-	if (window.jQuery === undefined || window.jQuery.fn.jquery < version) {
-		var done = false;
-		var script = document.createElement("script");
-		script.src = "https://ajax.googleapis.com/ajax/libs/jquery/" + version + "/jquery.min.js";
-		script.onload = script.onreadystatechange = function(){
-			if (!done && (!this.readyState || this.readyState == "loaded" || this.readyState == "complete")) {
-				done = true;
-				initBoxify(window.jQuery);
-			}
-            else {
-                console.log('error loading');
-            }
-		};
-		document.getElementsByTagName("head")[0].appendChild(script);
-        if (window.jQuery === undefined || window.jQuery.fn.jquery < version) {
-            console.log(':::');
-        }
-        else {
-            console.log('jquery loaded');
-        }
-	} else {
-		initBoxify(window.jQuery);
-	}
-	function initBoxify($) {
-		(window.boxify = function() {
-      var colors = ["red", "blue", "pink", "green", "yellow"];
-      var randomColor = Math.floor(Math.random() * colors.length);
-      var chosenColor = colors[randomColor];
-      $("*").css("border", "1px solid " + chosenColor);
+	let all = document.querySelectorAll(`*`);
 
-		})();
+	for (let i = 0; i < all.length; i++) {
+	  let colors = [`red`, `blue`, `lime`, `pink`, `yellow`, `black`, `cyan`, `magenta`, `blueviolet`];
+	  let randomColor = Math.floor(Math.random() * colors.length);
+	  let chosenColor = colors[randomColor];
+	  all[i].style.border =  `2px solid ${chosenColor}`;
 	}
 })();
